f2f9ab0c300e0b88b29d4ea571e60553637c8304
stella-ds <stella@dev2vec.com>
2021-09-25 21:05:12 +0000

    added examples to example_notebooks

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# examples
-example code for demoing features
+# Example notebooks
+You can find here a list of the official notebooks provided by Hyperplane.
+
+Also, we would like to list here interesting content created by the community. If you wrote some notebook(s) leveraging Hyperplane and would like be listed here, please open a Pull Request so it can be included under the Community notebooks.
+
+| Notebook         | Description                                         |
+| :----------------| :-------------------------------------------------- |
+| Distributed data prepcessing with dask  | A example of using distributed dask cluster within Jupyter notebooks on Hyperplane | 
---
+++ example_notebooks/inference/batch_inference/scheduler_setup.py
@@ -0,0 +1,81 @@
+"""
+startup script to be preloaded to the workers
+
+"""
+import pip._internal as pip
+def install(package):
+    pip.main(['install', package])
+
+try:
+    import s3fs
+except ImportError:
+    install('s3fs')
+    import s3fs
+
+import gcsfs
+import os
+import pickle
+from transformers import DistilBertTokenizer, TFDistilBertForSequenceClassification
+
+
+
+## start up script
+
+def download_from_cloud(local_file_name, remote_file_name):
+    """
+    Download a file to gcp or s3.
+    """
+    import os
+    import s3fs
+    import gcsfs
+    cloud_name = remote_file_name.split('://')[0]
+    if cloud_name =='gs':
+        fs = gcsfs.GCSFileSystem(project=os.environ['GCP_PROJECT'])
+    elif cloud_name =='s3':
+        fs = s3fs.S3FileSystem()
+    else:
+        raise NameError(f'cloud name {cloud_name} unknown')
+    try:    
+        print(f'download from {remote_file_name} to {local_file_name}')
+        fs.get(remote_file_name, local_file_name, recursive=True)
+        print("done downloading!")
+    except Exception as exp:
+        print(f"download failed: {exp}")
+
+    return
+
+
+def load_models(model_path):
+    """
+    Load the model from the unzipped local model path
+    """
+
+    model = TFDistilBertForSequenceClassification.from_pretrained(f'{model_path}/clf')
+    model_name, max_len = pickle.load(open(f'{model_path}/info.pkl', 'rb'))
+    loaded_models['model'] = (model, model_name, max_len)
+
+    return loaded_models
+
+
+def load_model_from_pretrained(model_name):
+    """
+    Load the model from the unzipped local model path
+    """
+
+    model = TFDistilBertForSequenceClassification.from_pretrained(model_name)
+    max_len = 20
+    loaded_models['model'] = (model, model_name, max_len)
+
+    return loaded_models
+
+# local_model_path = '/root/model'
+# remote_file_path = "s3://d2v-tmp/demo/bach_inference/model"
+# remote_file_path = "gs://pipeline_data/demo/bach_inference/model.zip"
+# download_from_cloud(local_model_path, remote_file_path)
+# loaded_models = {}
+# loaded_models = load_models(local_model_path)
+
+
+loaded_models = {}
+model_name = "distilbert-base-uncased"
+loaded_models = load_model_from_pretrained(model_name)
